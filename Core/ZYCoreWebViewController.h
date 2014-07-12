b47f915836bda8214a4c41584f6e6646c48ec736
Xiao Du <zengyun.programmer@gmail.com>
2014-07-13 00:38:07 +0800

    feature:(webview)完成webview

---
+++ Core/ZYCoreWebViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZYCoreWebViewController.h
+//  ZYCoreFramework
+//
+//  Created by dreamer on 14-7-13.
+//  Copyright (c) 2014年 SnowWolf. All rights reserved.
+//
+
+#import "ZYCoreViewController.h"
+
+@interface ZYCoreWebViewController : ZYCoreViewController
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) NSString *targetUrl;
+
+@end
---
+++ Core/ZYCoreWebViewController.m
@@ -0,0 +1,56 @@
+//
+//  ZYCoreWebViewController.m
+//  ZYCoreFramework
+//
+//  Created by dreamer on 14-7-13.
+//  Copyright (c) 2014年 SnowWolf. All rights reserved.
+//
+
+#import "ZYCoreWebViewController.h"
+#import "ZYConfig.h"
+
+@interface ZYCoreWebViewController ()
+
+@end
+
+@implementation ZYCoreWebViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)addWebView {
+    self.webView = [[UIWebView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.webView];
+    if (self.targetUrl && self.targetUrl.length > 0) {
+        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.targetUrl]]];
+    }
+}
+
+- (void)back {
+    if ([self.webView canGoBack]) {
+        [self.webView goBack];
+    }
+    else {
+        [self.navigationController popToRootViewControllerAnimated:CONFIG_ANIMATION_POP];
+    }
+}
+
+@end
